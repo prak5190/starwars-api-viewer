a2b88e2ddc8577a0e02bd5686c9f270e6cbfb2c2
Prakash <prakraja@umail.iu.edu>
2015-09-12 23:58:47 -0400

    Adding jQuery and bootstrap. Doing initial render of table data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,71 @@
 import React from 'react'
 
+
+function get(url) {
+    return $.ajax({url,type: 'json',method:'get'})
+}
+
+
+var URL = {
+    "starships" : "http://swapi.co/api/starships/",
+    "pilot": (id) => `http://swapi.co/api/people/${id}/`,    
+}
+
+
+const Ship = React.createClass({
+    render() {
+        var x = this.props.ship ;
+        return (
+            <tr>
+            <td onClick={this.props.onClick}>{x.name}</td><td>{x.model}</td><td>{x.cost_in_credits}</td>
+            </tr>)
+    }
+});
+
+const StarWarsShips = React.createClass({
+    getInitialState() {        
+        return { priceSort : 0, ships : []};
+    },
+    componentDidMount() {
+        var self = this ;
+        this.state.ships
+        // Get all data in one go - Need it for sorting and filtering
+        var update = function(url) {            
+            get(url).then(function(x) {                
+                var arr = self.state.ships ;
+                arr.push.apply(arr,x.results);
+                self.setState({ships : arr});
+                if (x.next)
+                    update(x.next);
+            });
+        };
+        update(URL.starships);
+    },
+    render() {
+        var arr = this.state.ships;        
+        var rows = arr.map((x)=> <Ship key={x.name} ship={x} />);
+        return (            
+            <div>
+            <table>
+            <thead>
+            <td>Name</td><td>Model</td><td>Price</td>  
+            </thead>
+            <tbody>
+            {rows}
+            </tbody>
+            </table>            
+            </div>
+        )            
+    }
+});
+
 const StartWars = React.createClass({
   render() {
-    return (
-      <h1>Make me do Star Wars stuff!</h1>
+      return (
+          <div>
+          <h1>Make me do Star Wars stuff!</h1>
+          <StarWarsShips />
+          </div>
     )
   }
 })
